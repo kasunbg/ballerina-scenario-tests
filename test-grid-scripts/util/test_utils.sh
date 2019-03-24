4ce447f37b70e1369aba141a890913428157f58d
Manuri <amaya.uom@gmail.com>
2019-03-24 13:17:22 +0530

    Fix paths

--- test-grid-scripts/util/test_utils.sh
+++ test-grid-scripts/util/test_utils.sh
@@ -37,9 +37,9 @@ run_bbg_section_tests() {
     bash --version
     for x in "${!properties_array[@]}"; do sys_prop_str+="-D$x=${properties_array[$x]} " ; done
 
-    mvn clean install -f ${utils_great_grand_parent_path}/pom.xml -fae -Ddata.bucket.location=${__input_dir} ${sys_prop_str} -Dtestng.groups=${test_group} -P ${maven_profile}
+    mvn clean install -f ${test_utils_great_grand_parent_path}/pom.xml -fae -Ddata.bucket.location=${__input_dir} ${sys_prop_str} -Dtestng.groups=${test_group} -P ${maven_profile}
 
     mkdir -p ${__output_dir}/scenarios
 
-    cp -r ${utils_great_grand_parent_path}/bbg/${bbg_section}/target ${__output_dir}/scenarios/${bbg_section}/
+    cp -r ${test_utils_great_grand_parent_path}/bbg/${bbg_section}/target ${__output_dir}/scenarios/${bbg_section}/
 }
